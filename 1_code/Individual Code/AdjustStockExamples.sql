8ad4958f865f12dd5cc3dbab1be0fe6e52b0079b
CDerrick0426 <79726299+CDerrick0426@users.noreply.github.com>
2024-12-05 04:06:40 -0500

    Update AdjustStockExamples.sql

--- 1_code/Individual Code/AdjustStockExamples.sql
+++ 1_code/Individual Code/AdjustStockExamples.sql
@@ -1,5 +1,12 @@
 -- To use the procedure, type in the product ID, location, and how much to add.
 -- Examples given below.
 
-CALL AdjustStock(productID, 'sales_floor', stockAmount); 
-CALL AdjustStock(productID, 'reserve', stockAmount);
+CALL AdjustStock(productID, 'sales_floor', stockAmount, reasonID); 
+CALL AdjustStock(productID, 'reserve', stockAmount, reasonID);
+
+-- Reason ID List
+-- 10: Normal Restock
+-- 11: Expired Products
+-- 12: Broken Items
+-- 13: Missing Items
+-- 14: Excessive Demand

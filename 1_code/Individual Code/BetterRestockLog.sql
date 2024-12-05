de61315484e9f0997c9bed5cfd6023f76f1a888f
CDerrick0426 <79726299+CDerrick0426@users.noreply.github.com>
2024-12-05 04:07:37 -0500

    Update BetterRestockLog.sql

--- 1_code/Individual Code/BetterRestockLog.sql
+++ 1_code/Individual Code/BetterRestockLog.sql
@@ -1,14 +1,19 @@
-CREATE VIEW BetterRestockLog AS
+CREATE OR REPLACE VIEW BetterRestockLog AS
 SELECT 
     rl.log_id,
     rl.product_id,
     p.name,
     rl.location,
     rl.quantity_added,
-    rl.updated_at
+    rl.updated_at,
+    rr.reason_description AS reason_name
 FROM 
     RestockLog rl
 JOIN 
     Products p
 ON 
-    rl.product_id = p.product_id;
+    rl.product_id = p.product_id
+JOIN 
+    RestockReasons rr
+ON 
+    rl.reason_id = rr.reason_id;
